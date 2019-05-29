48ae840874508bc56e690e04f8b5a0bcce7e5345
Abhishek <hritik6058@gmail.com>
2019-05-30 01:29:25 +0530

    Initial commit

---
+++ app/src/main/java/mtapp/demo/googlephoneverfication/MainActivity.kt
@@ -0,0 +1,105 @@
+package mtapp.demo.googlephoneverfication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity(){
+
+    private val TAG = "FirebasePhoneVerify"
+    private lateinit var auth:FirebaseAuth
+    private lateinit var callbacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:$credential")
+
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e)
+
+
+                if (e is FirebaseAuthInvalidCredentialsException) {
+                    toast("Invalid phone number.")
+                } else if (e is FirebaseTooManyRequestsException) {
+                    toast("Quota exceeded")
+                }
+
+            }
+
+            override fun onCodeSent(
+                verificationId: String?,
+                token: PhoneAuthProvider.ForceResendingToken
+            ) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId!!)
+            }
+        }
+
+        button.setOnClickListener {
+            startPhoneNumberVerification(phoneNumber.text.toString())
+        }
+
+    }
+    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+
+                    val user = task.result?.user
+
+                } else {
+                    // Sign in failed, display a message and update the UI
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                }
+            }
+    }
+
+    private fun startPhoneNumberVerification(phoneNumber: String) {
+        // [START start_phone_auth]
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber,      // Phone number to verify
+            60,               // Timeout duration
+            TimeUnit.SECONDS, // Unit of timeout
+            this,             // Activity (for callback binding)
+            callbacks) // OnVerificationStateChangedCallbacks
+        // [END start_phone_auth]
+    }
+
+    fun AppCompatActivity.toast(message:String){
+        Toast.makeText( this , message , Toast.LENGTH_LONG ).show()
+    }
+}
